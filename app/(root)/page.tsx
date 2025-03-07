74e25636c07708a635f1c897f2c4c5e1c30ff677
firoze-hossain <firoze.hossain@outlook.com>
2025-03-08 05:04:23 +0600

    headerbox and totalBance comp 1st stage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,28 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Firoze" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction effeciently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1500}
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
