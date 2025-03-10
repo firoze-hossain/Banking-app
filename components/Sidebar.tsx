90f8615ea0652d06529a996461d7f83bbdc38153
firoze-hossain <firoze.hossain@outlook.com>
2025-03-10 22:26:52 +0600

    active sidebar page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { sidebarLinks } from "@/constants";
+import { usePathname } from "next/navigation";
 
 const Sidebar = ({ user }: SiderbarProps) => {
+  const pathName = usePathname();
   return (
     <section className="sidebar">
       <nav className="flex flex-col gap-4">
@@ -19,8 +22,14 @@ const Sidebar = ({ user }: SiderbarProps) => {
           <h1 className="sidebar-logo">Roze</h1>
         </Link>
         {sidebarLinks.map((item) => {
+          const isActive =
+            pathName === item.route || pathName.startsWith(`${item.route}/`);
           return (
-            <Link href={item.route} key={item.label}>
+            <Link
+              href={item.route}
+              key={item.label}
+              className="{cn('sidebar-link',{'bg-bank-gradient':isActive})}"
+            >
               {item.label}
             </Link>
           );
