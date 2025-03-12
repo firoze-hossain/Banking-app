f8a6e70403bc5b9616e18aadd3a18794b66f5110
firoze-hossain <firoze.hossain@outlook.com>
2025-03-12 19:34:43 +0600

    class name and div section modified

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -7,7 +7,7 @@ const TotalBalanceBox = ({
   accounts = [],
   totalBanks,
   totalCurrentBalance,
-}: TotlaBalanceBoxProps) => {
+}: TotalBalanceBoxProps) => {
   return (
     <section className="total-balance">
       <div className="total-balance-chart">
@@ -15,11 +15,12 @@ const TotalBalanceBox = ({
       </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
-      </div>
-      <div className="flex flex-col gap-2">
-        <p className="total-balance-label">Total current Balance</p>
-        <div className="total-balance-amount flex-center gap-2">
-          <AnimatedCounter amount={totalCurrentBalance} />
+
+        <div className="flex flex-col gap-2">
+          <p className="total-balance-label">Total current Balance</p>
+          <div className="total-balance-amount flex-center gap-2">
+            <AnimatedCounter amount={totalCurrentBalance} />
+          </div>
         </div>
       </div>
     </section>
