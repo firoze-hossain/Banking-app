abab67266ab6118f862a9d84b74a7ee3fef65dca
firoze-hossain <firoze.hossain@outlook.com>
2025-03-16 21:51:07 +0600

    code refactor for auth

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-const CustomInput = () => {
-  return <div>CustomInput</div>;
+import { Input } from "@/components/ui/input";
+import { Control, Form } from "react-hook-form";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { authFormSchema } from "@/lib/utils";
+interface CustomInput {
+  control: Control<z.infer<typeof authFormSchema>>;
+  name: string;
+  label: string;
+  placeholder: string;
+}
+const CustomInput = ({ control, name, label, placeholder }: CustomInput) => {
+  return (
+    <FormField
+      control={control}
+      name="name"
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex w-full flex-col">
+            <FormControl>
+              <Input
+                placeholder={placeholder}
+                className="input-class"
+                {...field}
+              ></Input>
+            </FormControl>
+            <FormMessage className="form-message mt-2"></FormMessage>
+          </div>
+        </div>
+      )}
+    />
+  );
 };
 
 export default CustomInput;
